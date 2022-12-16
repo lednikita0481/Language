a91816c31983aa87bf2a1fa99b3dbadf2a737b27
lednikita0481 <lednikita0481@gmail.com>
2022-12-16 13:57:13 +0300

    Initial commit

---
+++ language.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include "dictionary.h"
+#include "language.h"
+
+
+//--------------------------------Tokeniser------------------------------------
